2a31ff30ba8991e867ad90236278987015607f3c
jinst3r <choi.jonathanh@gmail.com>
2015-06-09 19:42:51 -0700

    setup

---
+++ nearbuy/nearbuy/ItemCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  ItemCollectionViewCell.swift
+//  nearbuy
+//
+//  Created by Jon Choi on 6/9/15.
+//  Copyright (c) 2015 Marcus J. Ellison. All rights reserved.
+//
+
+import UIKit
+
+class ItemCollectionViewCell: UICollectionViewCell {
+    
+}
