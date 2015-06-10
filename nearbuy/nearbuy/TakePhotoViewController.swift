6e71f8f78a34305a3ee173e3980e4b03c7621f16
Marcus J. Ellison <marcus@wurkhappy.com>
2015-06-09 20:55:22 -0700

    rough take photo controller

--- nearbuy/nearbuy/TakePhotoViewController.swift
+++ nearbuy/nearbuy/TakePhotoViewController.swift
@@ -7,9 +7,12 @@
 //
 
 import UIKit
+import MobileCoreServices
 
-class TakePhotoViewController: UIViewController {
+class TakePhotoViewController: UIViewController, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
 
+    @IBOutlet weak var productImageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -31,5 +34,69 @@ class TakePhotoViewController: UIViewController {
         // Pass the selected object to the new view controller.
     }
     */
+    @IBAction func takePhoto(sender: AnyObject) {
+        if (UIImagePickerController.isSourceTypeAvailable(UIImagePickerControllerSourceType.Camera)){
+            var picker = UIImagePickerController()
+            picker.delegate = self
+            picker.sourceType = UIImagePickerControllerSourceType.Camera
+            picker.mediaTypes = [kUTTypeImage]
+            picker.allowsEditing = true
+            self.presentViewController(picker, animated: true, completion: nil)
+        }
+        else{
+            NSLog("No Camera.")
+            var alert = UIAlertController(title: "No camera", message: "Please allow this app the use of your camera in settings or buy a device that has a camera.", preferredStyle: UIAlertControllerStyle.Alert)
+            alert.addAction(UIAlertAction(title: "Dismiss", style: UIAlertActionStyle.Default, handler: nil))
+            self.presentViewController(alert, animated: true, completion: nil)
+        }
+    }
+    
+    @IBAction func uploadPhoto(sender: AnyObject) {
+        if (UIImagePickerController.isSourceTypeAvailable(UIImagePickerControllerSourceType.PhotoLibrary)) {
+            var picker = UIImagePickerController()
+            picker.delegate = self
+            picker.allowsEditing = true
+            picker.sourceType = UIImagePickerControllerSourceType.PhotoLibrary
+            
+            self.presentViewController(picker, animated: true, completion: nil)
+            
+        } else {
+            NSLog("no photo library.")
+            var alert = UIAlertController(title: "No photo library", message: "Please allow this app the use of your photo library in settings or buy a device that has a photo library.", preferredStyle: UIAlertControllerStyle.Alert)
+            alert.addAction(UIAlertAction(title: "Dismiss", style: UIAlertActionStyle.Default, handler: nil))
+            self.presentViewController(alert, animated: true, completion: nil)
+            
+        }
+        
+        func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: NSDictionary!) {
+            NSLog("Received image from camera")
+            
+            let mediaType = info[UIImagePickerControllerMediaType] as! String
+            println(mediaType)
+            var originalImage:UIImage?, editedImage:UIImage?, imageToSave:UIImage?
+            let compResult:CFComparisonResult = CFStringCompare(mediaType as NSString!, kUTTypeImage, CFStringCompareFlags.CompareCaseInsensitive)
+            if ( compResult == CFComparisonResult.CompareEqualTo ) {
+                
+                editedImage = info[UIImagePickerControllerEditedImage] as! UIImage?
+                originalImage = info[UIImagePickerControllerOriginalImage] as! UIImage?
+                
+                if ( editedImage != nil ) {
+                    imageToSave = editedImage
+                } else {
+                    imageToSave = originalImage
+                }
+                self.productImageView.image = imageToSave
+                self.productImageView.reloadInputViews()
+            }
+            picker.dismissViewControllerAnimated(true, completion: nil)
+        }
+        
+        func imagePickerControllerDidCancel(picker: UIImagePickerController) {
+            picker.dismissViewControllerAnimated(true, completion: nil)
+        }
+        
+
+    }
+    
 
 }
