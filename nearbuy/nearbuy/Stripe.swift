17b83f807354620f42683434da48211735a102f4
Kavodel Ohiomoba <kohiomobz@gmail.com>
2015-06-14 22:18:25 -0700

    adding shyp and stripe classes - need to fill in

---
+++ nearbuy/nearbuy/Stripe.swift
@@ -0,0 +1,13 @@
+//
+//  Stripe.swift
+//  nearbuy
+//
+//  Created by Kavodel Ohiomoba on 6/14/15.
+//  Copyright (c) 2015 Marcus J. Ellison. All rights reserved.
+//
+
+import UIKit
+
+class Stripe: NSObject {
+   
+}
