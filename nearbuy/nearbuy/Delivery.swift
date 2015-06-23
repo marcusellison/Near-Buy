d828cdaab803c7095b323459c72febfb4c15e6a5
Kavodel Ohiomoba <kohiomobz@gmail.com>
2015-06-22 20:17:21 -0700

    adding changes delivery API code

--- nearbuy/nearbuy/Delivery.swift
+++ nearbuy/nearbuy/Delivery.swift
@@ -8,15 +8,36 @@
 
 import UIKit
 
+let postmatesCustomerID = "cus_KNNem7avnOEalF"
+let postmatesAPIKey = "fda8ba46-89c3-4959-8ad3-e0dc20ff1088"
+var postmatesAPIURL = "https://api.postmates.com/v1/customers" + postmatesCustomerID
+
 class Delivery: NSObject {
     /*  
-        Use the PostMates API to setup a 
+        Use the PostMates API to setup delivery
 
     */
     
-    /* var address: NSDictionary */
+    var buyerAddress: [String: String]?
+    var buyerName: String?
+    var buyerPhone: String?
+    var buyerNotes: String?
     
+    var sellerAddress: [String: String]?
+    var sellerName: String?
+    var sellerPhone: String?
+    var sellerNotes: String?
     
+    var manifest: String?
+    
+    func getDeliveryQuote(buyer: [String: String], sender: [String: String]){
+        
+        
+    }
+    
+    func makeDelivery(){
+        
+    }
     
    
 }
