8353a6e2713b02e470c20c4a3e25dbeb682b46d1
Marcus J. Ellison <marcus@wurkhappy.com>
2015-06-23 10:44:00 -0700

    added in some draft copy and view controllers for welcome screen

---
+++ nearbuy/nearbuy/WelcomeGetStartedViewController.swift
@@ -0,0 +1,35 @@
+//
+//  WelcomeGetStartedViewController.swift
+//  nearbuy
+//
+//  Created by Marcus J. Ellison on 6/23/15.
+//  Copyright (c) 2015 Marcus J. Ellison. All rights reserved.
+//
+
+import UIKit
+
+class WelcomeGetStartedViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
